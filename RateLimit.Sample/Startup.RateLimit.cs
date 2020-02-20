817f3f72a4fccfc0b23701826044d801a5937ee4
TonyChang <tony.chang@run-cloud.com.tw>
2020-02-20 16:36:53 +0800

    加入專案檔案。

---
+++ RateLimit.Sample/Startup.RateLimit.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreRateLimit;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace RateLimit.Sample
+{
+    public partial class Startup
+    {
+        public void AddIpLimit(IServiceCollection services)
+        {
+            //load general configuration from appsettings.json
+            services.Configure<IpRateLimitOptions>(Configuration.GetSection("RateLimiting"));
+
+            //load ip rules from appsettings.json
+            services.Configure<IpRateLimitPolicies>(Configuration.GetSection("IpRateLimitPolicies"));
+
+#if RELEASE
+            // inject counter and rules distributed cache stores (IDistributedCache DI required)
+            services.AddStackExchangeRedisCache(o =>
+            {
+                var config = new ConfigurationOptions()
+                {
+                    AbortOnConnectFail = true,
+                    Password = string.Empty,
+                    KeepAlive = 60,
+                    DefaultDatabase = 0,
+                    SyncTimeout = 3000,
+                    Ssl = false,
+                };
+                config.EndPoints.Add(Configuration["Connection:Redis"]);
+                o.ConfigurationOptions = config;
+            });
+            services.AddSingleton<IIpPolicyStore, DistributedCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, DistributedCacheRateLimitCounterStore>();
+#endif
+#if DEBUG
+            // inject counter and rules stores
+            services.AddMemoryCache();
+            services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+#endif
+            // configuration (resolvers, counter key builders)
+            services.AddHttpContextAccessor();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        }
+
+        public void AddClientLimit(IServiceCollection services)
+        {
+
+            //load general configuration from appsettings.json
+            services.Configure<ClientRateLimitOptions>(Configuration.GetSection("RateLimiting"));
+
+            //load client rules from appsettings.json
+            services.Configure<ClientRateLimitPolicies>(Configuration.GetSection("ClientRateLimitPolicies"));
+
+#if RELEASE
+            // inject counter and rules distributed cache stores (IDistributedCache DI required)
+            services.AddStackExchangeRedisCache(o =>
+            {
+                var config = new ConfigurationOptions()
+                {
+                    AbortOnConnectFail = true,
+                    Password = string.Empty,
+                    KeepAlive = 60,
+                    DefaultDatabase = 0,
+                    SyncTimeout = 3000,
+                    Ssl = false,
+                };
+                config.EndPoints.Add(Configuration["Connection:Redis"]);
+                o.ConfigurationOptions = config;
+            });
+            services.AddSingleton<IClientPolicyStore, DistributedCacheClientPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, DistributedCacheRateLimitCounterStore>();
+#endif
+#if DEBUG
+            // inject counter and rules stores
+            services.AddMemoryCache();
+            services.AddSingleton<IClientPolicyStore, MemoryCacheClientPolicyStore>();
+            services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+#endif
+            // configuration (resolvers, counter key builders)
+            services.AddHttpContextAccessor();
+            services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+        }
+    }
+}
---
+++ RateLimit.Sample/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace RateLimit.Sample
+{
+    public partial class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            this.AddIpLimit(services);
+            this.AddClientLimit(services);
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // activate ip rate limiting
+            app.UseIpRateLimiting();
+            // activate client rate limiting
+            app.UseClientRateLimiting();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
